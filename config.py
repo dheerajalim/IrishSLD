da422d949fc4578f1c704880c6019cbdc0297bca
dheeraj <dheeraj.alim@gmail.com>
2020-02-24 23:38:18 +0000

    Added a configuration file for the config variables

---
+++ config.py
@@ -0,0 +1,24 @@
+"""
+Filename : config.py
+Author : Dheeraj Alimchandani
+Date : 20-02-2020
+Usage : Contains the configuration items
+"""
+
+CAMERA = 'http://192.168.2.2:4747/video'
+
+GESTURE_DESTINATION = 'irish_sign_language_gestures'
+
+KERAS_PATH = 'keras_cnn_model.h5'
+
+GESTURE_DB = 'irish_sign_language_gestures_db.db'
+
+GESTURE_COUNT = 1000
+
+IMAGE_DIMENSION_X = 50
+
+IMAGE_DIMENSION_Y = 50
+
+LOWER_BOUND = 'lower_bound.npy'
+
+UPPER_BOUND = 'upper_bound.npy'
