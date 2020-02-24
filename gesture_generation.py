62d9a56bec0b33d9cf9d50262f05f945ac864f7a
dheeraj <dheeraj.alim@gmail.com>
2020-02-24 23:54:31 +0000

    1. used utility functions to improve the script 2. Used configuration variables instead of hardcoded variables 3. Added alerts for storing the gestures 4. Added condition checks for the gesture information 4. Added text on the video frame for giving information to user

--- gesture_generation.py
+++ gesture_generation.py
@@ -5,11 +5,13 @@ Date : 12-02-2020
 Usage : Generation of gesture dataset
 """
 
-import cv2
-import numpy as np
+# import cv2
+# import numpy as np
 import sqlite3
 import os
 import random
+import pymsgbox
+from frame_utils import *
 
 
 class GestureCreation:
@@ -18,16 +20,17 @@ class GestureCreation:
 
         self.gesture_id = None  # The Id for the gesture
         self.gesture_detail = None  # The details for the gesture
-        self.gesture_destination = 'irish_sign_language_gestures'  # Gesture storage directory name
-        self.gesture_db = 'irish_sign_language_gestures_db.db'  # Database
+        self.gesture_destination = GESTURE_DESTINATION  # Gesture storage directory name
+        self.gesture_db = GESTURE_DB  # Database
         self.cap = None
         self.kernel = None
-        self.gesture_count = 1000  # Number of gestures to be captured
+        self.gesture_count = GESTURE_COUNT  # Number of gestures to be captured
         self.capture_no = 0
         self.start_capturing_gesture = False
         self.frame_count = 0
-        self.image_dimension_x = 50  # Final image width
-        self.image_dimension_y = 50  # Final image height
+        self.image_dimension_x = IMAGE_DIMENSION_X  # Final image width
+        self.image_dimension_y = IMAGE_DIMENSION_Y  # Final image height
+
 
     def gesture_details_database(self):
         """
@@ -52,8 +55,23 @@ class GestureCreation:
         Capturing the details of the gesture, gesture ID and Gesture details and inserting it into the database
         :return: None
         """
-        self.gesture_id = input('Gesture Number')
-        self.gesture_detail = input('Gesture Details')
+        ''' taking gesture id and checking it should be a valid number and exiting the program on user cancellation'''
+        try:
+            id_value = pymsgbox.prompt('Gesture Number', default='', title='Gesture Information')
+            self.gesture_id = int(-1 if id_value is None else id_value)
+        except (ValueError, TypeError) as e:
+            pymsgbox.alert('Please enter a valid integer ID', 'Alert!')
+            self.gesture_details_data()
+            return
+        if self.gesture_id is -1:
+            exit(0)
+        else:
+            self.gesture_id = str(self.gesture_id)
+
+        ''' taking gesture details and exiting the program on user cancellation'''
+        self.gesture_detail = pymsgbox.prompt('Gesture Details', default='', title='Gesture Information')
+        if self.gesture_detail is None:
+            exit(0)
 
         conn = sqlite3.connect(self.gesture_db)
         query = "INSERT INTO isl_gesture (gesture_id, gesture_detail) VALUES (%s, \'%s\')" % \
@@ -62,8 +80,9 @@ class GestureCreation:
         try:
             conn.execute(query)
         except sqlite3.IntegrityError:
-            choice = input("Gesture with this ID already exists. Want to change the record? (y/n): ")
-            if choice.lower() == 'y':
+            choice = pymsgbox.confirm('Gesture with this ID already exists. Want to change the record?',
+                                      'Confirm', ["Yes", 'No'])  # checking already existing gesture
+            if choice.lower() == 'yes':
                 cmd = "UPDATE isl_gesture SET gesture_detail" \
                       " = \'%s\' WHERE gesture_id = %s" % (self.gesture_detail, self.gesture_id)
                 conn.execute(cmd)
@@ -75,64 +94,35 @@ class GestureCreation:
         conn.commit()
 
     @staticmethod
-    def load_bounds():
-        """Using the lower and upper bounds saved from the hand_detection.py"""
-
-        lower_bound = np.load('lower_bound.npy')
-        upper_bound = np.load('upper_bound.npy')
-
-        return lower_bound, upper_bound
-
-    @staticmethod
     def gesture_directory(directory_name):
         """Create the directory for the new gesture"""
         if not os.path.exists(directory_name):
             os.mkdir(directory_name)
 
     def capture_gesture(self):
-        self.cap = cv2.VideoCapture('http://192.168.2.2:4747/video')  # Capturing the Video
-
-        if not self.cap.read()[0]:
-            self.cap = cv2.VideoCapture(0)
-
-        self.kernel = np.ones((5, 5), np.uint8)  # Creating the Kernel definition
-
+        self.cap = capture_video()  # Capturing the Video
+        self.kernel = morphological_kernel()  # Kernel for the morphological transformation
         self.gesture_directory(f'{self.gesture_destination}/{self.gesture_id}')  # Create gesture directory
 
         while True:
             _, img = self.cap.read()  # reading the captured frame
-            img = cv2.flip(img, 1)
-            # cv2.rectangle(img, (300, 100), (600, 400), (255, 0, 0)) # creating a rectangle on the
-            # cv2.rectangle(img, (30, 50), (330, 350), (255, 0, 0))  # creating a rectangle on the frame
-            cv2.rectangle(img, (300, 50), (600, 350), (255, 0, 0))
-            # roi = img[100:400, 300:600]
-            roi = img[50:350, 300:600]  # Getting the region of interest
-            # roi = cv2.flip(roi, 1)  # Flipping the image
+            img, roi = video_roi(img)
 
-            lower_bound, upper_bound = self.load_bounds()
+            cv2.putText(img, "Press C to Play and Pause capturing", (10, 460), cv2.FONT_HERSHEY_DUPLEX, 0.4, (0, 0, 0))
 
-            ycrcb_image = cv2.cvtColor(roi, cv2.COLOR_BGR2YCR_CB)  # changing BGR to YCrCb color
-            skin_region_mask = cv2.inRange(ycrcb_image, lower_bound, upper_bound)
-
-            skin = cv2.bitwise_and(roi, roi, mask=skin_region_mask)  # Performing bitwise and operation on the image
-
-            dilation = cv2.dilate(skin, self.kernel, iterations=2)
-            erode = cv2.erode(dilation, self.kernel, iterations=2)
-            median_blur = cv2.medianBlur(erode, 7)
-            skin = median_blur
+            lower_bound, upper_bound = load_bounds()
+            skin = frame_color_masking(roi, lower_bound, upper_bound)  # changing color and performing masking
+            skin = morphological_transformations(skin, self.kernel, 2, 7)  # Adding improvements over the detected skin
 
             '''generating contours'''
             edged = cv2.Canny(skin, 100, 255)
-            cv2.imshow('test', edged)
+            cv2.imshow('Canny Edge Image', edged)
             contours, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            # cv2.drawContours(skin, contours, -1, (0, 255, 0), 3)
-
             ''' generating contours ends'''
 
             if len(contours) > 0:  # Executing only if the contours are  greater than 0
 
                 contour = max(contours, key=cv2.contourArea)
-                # print(cv2.contourArea(contour))
                 if cv2.contourArea(contour) > 1000 and self.frame_count > 20:
                     x1, y1, w1, h1 = cv2.boundingRect(contour)
                     self.capture_no += 1
@@ -148,10 +138,10 @@ class GestureCreation:
                     if rand % 2 == 0:
                         save_img = cv2.flip(save_img, 1)
 
-                    cv2.putText(img, "Capturing...", (30, 60), cv2.FONT_HERSHEY_TRIPLEX, 2, (127, 255, 255))
+                    cv2.putText(img, "Capturing Gesture...", (10, 40), cv2.FONT_HERSHEY_DUPLEX, 0.7, (127, 255, 255))
                     cv2.imwrite(f'{self.gesture_destination}/{self.gesture_id}/{str(self.capture_no)}.jpg', save_img)
 
-            cv2.putText(img, str(self.capture_no), (30, 400), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (127, 127, 255))
+            cv2.putText(img, str(self.capture_no), (300, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 0))
 
             cv2.imshow('Image', img)  # Showing the real image
             cv2.imshow("Skin Toner", np.hstack([roi, skin]))  # Displaying the ROI and Skin Detected Image
@@ -166,8 +156,9 @@ class GestureCreation:
                     self.frame_count = 0
 
             if self.start_capturing_gesture:
+
                 self.frame_count += 1
-                print("C: " + str(self.frame_count))
+                # print("C: "+str(self.frame_count))
 
             if self.capture_no == self.gesture_count:
                 break
